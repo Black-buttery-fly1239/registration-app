e8ff1bf64c2f4b33c76dcd2939e60c86bee50315
nobu-hope <nobuhlelebz@gmail.com>
2021-10-18 16:02:15 +0200

    changes

--- test/database-test.js
+++ test/database-test.js
@@ -26,7 +26,9 @@ describe("The my_registration database", function () {
 		const theRegs = reg(pool);
 
 		await theRegs.regsAdd("CA 123456");
-		assert.deepEqual([{ regnumber: "CA 123456" }], await theRegs.list());
+		await theRegs.regsAdd("CY 123-456");
+		await theRegs.regsAdd("Ck 123-56");
+		assert.deepEqual([{ regnumber: "CA 123456" }, { regnumber: "CY 123-456" }, { regnumber: "CK 123-56" }], await theRegs.list());
 	});
 
 	// eslint-disable-next-line no-undef
@@ -35,7 +37,7 @@ describe("The my_registration database", function () {
 
 		await theRegs.regsAdd("CY 123-987");
 		await theRegs.regsAdd("cy 123-987");
-		assert.deepEqual([{ regnumber: "CY 123-987" }], await theRegs.checkReg());
+		assert.deepEqual([{ regnumber: "CY 123-987" }], await theRegs.list());
 	});
 
 	// eslint-disable-next-line no-undef
@@ -43,7 +45,8 @@ describe("The my_registration database", function () {
 		const theRegs = reg(pool);
     
 		await theRegs.regsAdd("CK 123456");
-		assert.deepEqual([{ regnumber: "CK 123456" }], await theRegs.show("CK"));
+		await theRegs.regsAdd("CK 876-987");
+		assert.deepEqual([{ regnumber: "CK 123456" }, { regnumber: "CK 876-987"}], await theRegs.show("CK"));
 	});
   
 	// eslint-disable-next-line no-undef
@@ -51,7 +54,8 @@ describe("The my_registration database", function () {
 		const theRegs = reg(pool);
     
 		await theRegs.regsAdd("CY 123-123");
-		assert.deepEqual([{ regnumber: "CY 123-123" }], await theRegs.show("CY"));
+		await theRegs.regsAdd("CY 777-99");
+		assert.deepEqual([{ regnumber: "CY 123-123" }, { regnumber: "CY 777-99"}], await theRegs.show("CY"));
 	});
   
 	// eslint-disable-next-line no-undef
@@ -59,7 +63,8 @@ describe("The my_registration database", function () {
 		const theRegs = reg(pool);
     
 		await theRegs.regsAdd("CA 124-124");
-		assert.deepEqual([{ regnumber: "CA 124-124" }], await theRegs.show("CA"));
+		await theRegs.regsAdd("CA 124646");
+		assert.deepEqual([{ regnumber: "CA 124-124" }, { regnumber: "CA 124646"}], await theRegs.show("CA"));
 	});
 
 
@@ -69,7 +74,9 @@ describe("The my_registration database", function () {
 		const theRegs = reg(pool);
     
 		await theRegs.regsAdd("CA 124-124");
-		assert.deepEqual([{ regnumber: "CA 124-124" }], await theRegs.viewAll());
+		await theRegs.regsAdd("CK 124124");
+		await theRegs.regsAdd("CY 222-76");
+		assert.deepEqual([{ regnumber: "CA 124-124" }, { regnumber: "CK 124124" }, { regnumber: "CY 222-76"}], await theRegs.viewAll());
 	});
   
 	// eslint-disable-next-line no-undef
